028a244a05ebf8686b4626813d87651be9ca1296
Jesus Marin <jesusangelm@gmail.com>
2021-10-07 16:41:56 -0400

    backgroundjob and data persistence

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  end if Rails.env.production?
+  mount Sidekiq::Web => '/sidekiq'
 end
